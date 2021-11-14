0661ec4a372a5944150ce131d2b49edf7622eb6c
eduar2gp <eduar2gp@gmail.com>
2021-11-14 14:35:48 -0500

    product interface updated

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -2,13 +2,7 @@ export interface IProduct {
     productId: string;
     productName: string;
     productDescription: string;
-<<<<<<< HEAD
-    stars: number;
-    logoUrl: string;
-}
-=======
     overview: string;
     stars: number;
     logoUrl: string;
 }
->>>>>>> eduardo
